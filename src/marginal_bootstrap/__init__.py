8d7f2d11fd75cd0d519a48a8d42f53a258051087
Ezequiel Leonardo Castaño <14986783+ELC@users.noreply.github.com>
2025-07-03 01:11:29 -0300

    Refactor code structure for improved readability and maintainability

--- src/marginal_bootstrap/__init__.py
+++ src/marginal_bootstrap/__init__.py
@@ -1,6 +1,13 @@
 from scipy.stats import gaussian_kde
 import numpy as np
 
+def silverman_bandwidth(x):
+    n = len(x)
+    std = np.std(x, ddof=1)
+    iqr = np.subtract(*np.percentile(x, [75, 25]))
+    sigma = min(std, iqr / 1.34)
+    return 0.9 * sigma * n ** (-1 / 5)
+
 def bootstrap_non_parametric(data, bootstrap_size=1000, random_generator=None):
     random_generator = random_generator or np.random.default_rng()
     n = len(data)
@@ -18,14 +25,17 @@ def bootstrap_bayesian(data, bootstrap_size=1000, random_generator=None):
 def bootstrap_with_jitter(data, bootstrap_size=1000, random_generator=None):
     random_generator = random_generator or np.random.default_rng()
     
+    silverman_h = silverman_bandwidth(data).reshape(-1, 1)
+
     n = len(data)
     indexes = random_generator.choice(n, size=(n, bootstrap_size), replace=True)
-    jitter = random_generator.normal(0, np.std(data) / 10, size=(n, bootstrap_size))
+    jitter = random_generator.normal(0, silverman_h, size=(n, bootstrap_size))
     return data[indexes] + jitter
 
 def bootstrap_kde(data, bootstrap_size=1000, random_generator=None):
     random_generator = random_generator or np.random.default_rng()
-    kde = gaussian_kde(data)
+    bandwidth = silverman_bandwidth(data) / np.std(data, ddof=1)
+    kde = gaussian_kde(data, bw_method=bandwidth)
     n = len(data)
     return np.array([
         kde.resample(n, seed=random_generator)
